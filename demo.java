3fc14366de13a1b250dfb909a0c42983b5b7470c
Ashborn14 <neon99156@gmail.com>
2023-08-24 15:36:41 +0530

    Initial Commit

---
+++ demo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class demo{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = sc.next();
+        System.out.println("Hello "+name);
+
+        sc.close();
+ 
+    }
+    
+}
